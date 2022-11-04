31ec198c48ea27a221f8264abcd63e98e0e1023d
oktaviaqa <oktaviaq.a@gmail.com>
2022-11-04 23:16:51 +0700

    mengubah .php

---
+++ detail_wisata.php
@@ -0,0 +1,405 @@
+<!DOCTYPE html>
+<html lang="en">
+  <title>Proyek Akhir wkwk</title>
+  <meta charset="utf-8" />
+  <meta
+    name="viewport"
+    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+  />
+
+  <link
+    href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700"
+    rel="stylesheet"
+  />
+  <link
+    href="https://fonts.googleapis.com/css?family=Alex+Brush"
+    rel="stylesheet"
+  />
+
+  <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous"
+    referrerpolicy="no-referrer"
+  />
+
+  <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css" />
+  <link rel="stylesheet" href="css/animate.css" />
+
+  <link rel="stylesheet" href="css/owl.carousel.min.css" />
+  <link rel="stylesheet" href="css/owl.theme.default.min.css" />
+  <link rel="stylesheet" href="css/magnific-popup.css" />
+
+  <link rel="stylesheet" href="css/aos.css" />
+
+  <link rel="stylesheet" href="css/ionicons.min.css" />
+
+  <link rel="stylesheet" href="css/bootstrap-datepicker.css" />
+  <link rel="stylesheet" href="css/jquery.timepicker.css" />
+
+  <link rel="stylesheet" href="css/flaticon.css" />
+  <link rel="stylesheet" href="css/icomoon.css" />
+  <link rel="stylesheet" href="css/style.css" />
+
+  <body>
+    <nav
+      class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light"
+      id="ftco-navbar"
+    >
+      <div class="container">
+        <a class="navbar-brand" href="index.php">LoGo.</a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#ftco-nav"
+          aria-controls="ftco-nav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="oi oi-menu"></span> Menu
+        </button>
+
+        <div class="collapse navbar-collapse" id="ftco-nav">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a href="index.php" class="nav-link">BERANDA</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                href="#"
+                class="nav-link dropdown-toggle"
+                id="navbarDarkDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                >KATEGORI</a
+              >
+              <ul
+                class="dropdown-menu dropdown-menu-dark"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+              <li><a class="dropdown-item" href="alam.php">Wisata Alam</a></li>
+              <li><a class="dropdown-item" href="alam.php">Wisata Religi</a></li>
+              <li><a class="dropdown-item" href="alam.php">Wisata Sejarah</a></li>
+              <li><a class="dropdown-item" href="alam.php">Wisata Keluarga</a></li>
+              <li><a class="dropdown-item" href="alam.php">Wisata Kuliner</a></li>
+              </ul>
+            </li>
+            <!-- <li class="nav-item active">
+              <a href="kategori.php" class="nav-link">KATEGORI</a>
+            </li> -->
+            <li class="nav-item active">
+              <a href="informasi.php" class="nav-link">INFORMASI</a>
+            </li>
+            <!-- <li class="nav-item"><a href="hotel.php" class="nav-link">Rute</a></li>
+         <li class="nav-item"><a href="blog.php" class="nav-link">Blog</a></li>
+        <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li> -->
+            <li class="nav-item cta">
+              <a href="rute.php" class="nav-link"><span>RUTE</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    
+    <div
+      class="hero-wrap"
+      style="background-image: url('images/bg_1.jpg'); height: 60vh"
+    >
+      <div class="overlay"></div>
+      <div class="container-fluid">
+          <div class="row" style="padding-top: 13rem;">
+            <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+              <div class="destination">
+                <a
+                  href="detail_wisata.php"
+                  class="img img-2 d-flex justify-content-center align-items-center"
+                  style="background-image: url(images/hotel-1.jpg)"
+                >
+                  <div
+                    class="icon d-flex justify-content-center align-items-center"
+                  >
+                    <span class="icon-search2"></span>
+                  </div>
+                </a>
+                <div class="text p-3">
+                  <div class="d-flex">
+                    <div class="one">
+                      <h3><a href="#">Hotel, Italy</a></h3>
+                      <p class="rate">
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star-o"></i>
+                        <span>8 Rating</span>
+                      </p>
+                    </div>
+                    <div class="two">
+                      <span class="price per-price"
+                        >$40<br /><small>/night</small></span
+                      >
+                    </div>
+                  </div>
+                  <p>
+                    Far far away, behind the word mountains, far from the
+                    countries
+                  </p>
+                  <hr />
+                  <p class="bottom-area d-flex">
+                    <span><i class="icon-map-o"></i> Miami, Fl</span>
+                    <span class="ml-auto"><a href="#">Detail</a></span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+              <div class="destination">
+                <a
+                  href="detail_wisata.php"
+                  class="img img-2 d-flex justify-content-center align-items-center"
+                  style="background-image: url(images/hotel-2.jpg)"
+                >
+                  <div
+                    class="icon d-flex justify-content-center align-items-center"
+                  >
+                    <span class="icon-search2"></span>
+                  </div>
+                </a>
+                <div class="text p-3">
+                  <div class="d-flex">
+                    <div class="one">
+                      <h3><a href="#">Hotel, Italy</a></h3>
+                      <p class="rate">
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star-o"></i>
+                        <span>8 Rating</span>
+                      </p>
+                    </div>
+                    <div class="two">
+                      <span class="price per-price"
+                        >$40<br /><small>/night</small></span
+                      >
+                    </div>
+                  </div>
+                  <p>
+                    Far far away, behind the word mountains, far from the
+                    countries
+                  </p>
+                  <hr />
+                  <p class="bottom-area d-flex">
+                    <span><i class="icon-map-o"></i> Miami, Fl</span>
+                    <span class="ml-auto"><a href="detail_wisata.php">Detail</a></span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="col-sm col-md-6 col-lg-4 ftco-animate">
+              <div class="destination">
+                <a
+                  href="#"
+                  class="img img-2 d-flex justify-content-center align-items-center"
+                  style="background-image: url(images/hotel-3.jpg)"
+                >
+                  <div
+                    class="icon d-flex justify-content-center align-items-center"
+                  >
+                    <span class="icon-search2"></span>
+                  </div>
+                </a>
+                <div class="text p-3">
+                  <div class="d-flex">
+                    <div class="one">
+                      <h3><a href="#">Hotel, Italy</a></h3>
+                      <p class="rate">
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star"></i>
+                        <i class="icon-star-o"></i>
+                        <span>8 Rating</span>
+                      </p>
+                    </div>
+                    <div class="two">
+                      <span class="price per-price"
+                        >$40<br /><small>/night</small></span
+                      >
+                    </div>
+                  </div>
+                  <p>
+                    Far far away, behind the word mountains, far from the
+                    countries
+                  </p>
+                  <hr />
+                  <p class="bottom-area d-flex">
+                    <span><i class="icon-map-o"></i> Miami, Fl</span>
+                    <span class="ml-auto"><a href="detail_wisata.php">Detail</a></span>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row d-flex">
+          <div class="col-sm col-md-12 col-lg-6 ftco-animate">
+            <div class="blog-entry align-self-stretch">
+              <a
+                href="alam.php"
+                class="block-20"
+                style="background-image: url('images/image_1.jpg');"
+              >
+              </a>
+              <div class="text p-4 d-block">
+                <!-- <span class="tag">Tips, Travel</span> -->
+                <h3 class="heading mt-3">
+                  <a href="alam.php"
+                    >Wisata Alam</a
+                  >
+                </h3>
+                <div class="meta mb-3">
+                  <div><a href="#">Ekowisata Mangrove</a></div>
+                  <div><a href="#">Kenpark</a></div>
+                  <div><a href="#">Hutan Bambu Keputih</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm col-md-12 col-lg-6  ftco-animate">
+            <div class="blog-entry align-self-stretch">
+              <a
+                href="alam.php"
+                class="block-20"
+                style="background-image: url('images/image_2.jpg')"
+              >
+              </a>
+              <div class="text p-4">
+                <!-- <span class="tag">Culture</span> -->
+                <h3 class="heading mt-3">
+                  <a href="alam.php"
+                    >Wisata Religi</a
+                  >
+                </h3>
+                <div class="meta mb-3">
+                  <div><a href="#">Masjid Sunan Ampel</a></div>
+                  <div><a href="#">Masjid Al-Akbar</a></div>
+                  <div><a href="#">Masjid Muhammad Cheng Hoo</a></div>
+                  <!-- <div>
+                    <a href="#" class="meta-chat"
+                      ><span class="icon-chat"></span> 3</a
+                    >
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm col-md-12 col-lg-6  ftco-animate">
+            <div class="blog-entry align-self-stretch">
+              <a
+                href="alam.php"
+                class="block-20"
+                style="background-image: url('images/image_3.jpg')"
+              >
+              </a>
+              <div class="text p-4">
+                <!-- <span class="tag">Tips, Travel</span> -->
+                <h3 class="heading mt-3">
+                  <a href="alam.php"
+                    >Wisata Sejarah</a
+                  >
+                </h3>
+                <div class="meta mb-3">
+                  <div><a href="#">Museum Pendidikan</a></div>
+                  <div><a href="#">Museum Perjuangan 10 Nopember</a></div>
+                  <div><a href="#">Monumen Kapal Selam</a></div>
+                  <div><a href="#">Museum House of Sampoerna</a></div>
+                  <!-- <div>
+                    <a href="#" class="meta-chat"
+                      ><span class="icon-chat"></span> 3</a
+                    >
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-sm col-md-12 col-lg-6  ftco-animate">
+            <div class="blog-entry align-self-stretch">
+              <a
+                href="alam.php"
+                class="block-20"
+                style="background-image: url('images/image_3.jpg')"
+              >
+              </a>
+              <div class="text p-4">
+                <!-- <span class="tag">Tips, Travel</span> -->
+                <h3 class="heading mt-3">
+                  <a href="alam.php"
+                    >Wisata Keluarga</a
+                  >
+                </h3>
+                <div class="meta mb-3">
+                  <div><a href="#">Taman Bungkul</a></div>
+                  <div><a href="#">Kebun Binatang Surabaya</a></div>
+                  <!-- <div>
+                    <a href="#" class="meta-chat"
+                      ><span class="icon-chat"></span> 3</a
+                    >
+                  </div> -->
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+          </div>
+          </div>
+      </div>
+    </div>
+    <!-- loader -->
+    <div id="ftco-loader" class="show fullscreen">
+      <svg class="circular" width="48px" height="48px">
+        <circle
+          class="path-bg"
+          cx="24"
+          cy="24"
+          r="22"
+          fill="none"
+          stroke-width="4"
+          stroke="#eeeeee"
+        />
+        <circle
+          class="path"
+          cx="24"
+          cy="24"
+          r="22"
+          fill="none"
+          stroke-width="4"
+          stroke-miterlimit="10"
+          stroke="#F96D00"
+        />
+      </svg>
+    </div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/aos.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/bootstrap-datepicker.js"></script>
+    <script src="js/jquery.timepicker.min.js"></script>
+    <script src="js/scrollax.min.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+    <script src="js/google-map.js"></script>
+    <script src="js/main.js"></script>
+  </body>
+</html>
